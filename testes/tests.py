90ddfe702561a7262846297796252fc5cba96d03
Gabrielsldev <gabrielsl.dev@gmail.com>
2021-07-19 11:11:58 -0300

    Adicionado testes

--- testes/tests.py
+++ testes/tests.py
@@ -19,22 +19,3 @@ def test_onemindata_timedelta():
 	assert (onemindata1[2].minute - onemindata1[0].minute) == 1
 	assert (onemindata2[2].minute - onemindata2[0].minute) == 1
 	assert (onemindata2[0].minute - onemindata1[0].minute) == 1 or -1
-
-
-
-
-
-# def test_uppercase():
-#     assert "loud noises".upper() == "LOUD NOISES"
-
-# def test_reversed():
-#     assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
-
-# def test_some_primes():
-#     assert 37 in {
-#         num
-#         for num in range(1, 50)
-#         if num != 1 and not any([num % div == 0 for div in range(2, num)])
-#     }
-
-
